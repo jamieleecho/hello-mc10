c46e4fdbdecc362638f0eaf7c1a0774a1ba22d6b
Jamie Cho <jamie@ginkgobioworks.com>
2021-02-07 21:47:03 -0500

    Use bastoc10

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #/usr/bin/bash
 
-for each in *.bas; do mcbasic $each && tasm6801 ${each%.*}.asm; done
+for each in *.bas; do
+	mcbasic $each
+	tasm6801 ${each%.*}.asm
+	bastoc10 $each $each.c10
+done
